a0db8469e49c9e9013cbe17ab57f71933c530015
Mike Hall <mikehall.outbox@gmail.com>
2021-10-14 11:09:14 -0500

    Fixed no header issues for instagram

--- SocialPostScheduler/Instagram/PostFromFeed.cs
+++ SocialPostScheduler/Instagram/PostFromFeed.cs
@@ -1,9 +1,12 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.ServiceModel.Syndication;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -42,91 +45,115 @@ namespace SocialPostScheduler
                         return;
                     }
 
-                    string feedUrl = Properties.Settings.Default.WebsiteFeed;
+                    HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(Properties.Settings.Default.WebsiteFeed);
+                    hwr.Accept = "text/xml, */*";
+                    hwr.Headers.Add(HttpRequestHeader.AcceptLanguage, "en-us");
+                    hwr.UserAgent = "Mozilla";
+                    hwr.KeepAlive = true;
+                    hwr.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
 
-                    using (XmlReader feedReader = XmlReader.Create(feedUrl))
+                    using (var resp = (HttpWebResponse)hwr.GetResponse())
                     {
-                        SyndicationFeed feedContent = SyndicationFeed.Load(feedReader);
-                        List<SyndicationItem> feedItems = new List<SyndicationItem>();
-
-                        foreach (SyndicationItem item in feedContent.Items)
+                        using (Stream s = resp.GetResponseStream())
                         {
-                            feedItems.Add(item);
-                        }
-                        feedItems.Reverse();
-                        feedContent.Items = feedItems;
+                            string cs = String.IsNullOrEmpty(resp.CharacterSet) ? "UTF-8" : resp.CharacterSet;
+                            Encoding e = Encoding.GetEncoding(cs);
 
-                        if (null == feedContent)
-                        {
-                            return;
-                        }
-
-                        foreach (SyndicationItem item in feedContent.Items)
-                        {
-                            string quota_total = string.Empty;
-                            string quota_usage = string.Empty;
-                            try
+                            using (StreamReader sr = new StreamReader(s, e))
                             {
-                                using (WebClient wb = new WebClient())
-                                {
-                                    string responseInString = wb.DownloadString("https://graph.facebook.com/" + Properties.Settings.Default.InstagramPageID + "/content_publishing_limit?fields=config&access_token=" + Properties.Settings.Default.InstagramToken);
+                                var allXml = sr.ReadToEnd();
 
-                                    JObject jdata = JObject.Parse(responseInString);
-                                    JToken array = jdata["data"];
-                                    foreach (JToken info in array)
+                                allXml = Regex.Replace(allXml,
+                                                        "(.*)<script type='text/javascript'>.+?</script>(.*)",
+                                                        "$1$2",
+                                                        RegexOptions.Singleline);
+
+                                using (XmlReader xmlr = XmlReader.Create(new StringReader(allXml)))
+                                {
+                                    SyndicationFeed feedContent = SyndicationFeed.Load(xmlr);
+                                    List<SyndicationItem> feedItems = new List<SyndicationItem>();
+
+                                    foreach (SyndicationItem item in feedContent.Items)
                                     {
-                                        quota_total = info["config"]["quota_total"].ToString();
+                                        feedItems.Add(item);
+                                    }
+                                    feedItems.Reverse();
+                                    feedContent.Items = feedItems;
+
+                                    if (null == feedContent)
+                                    {
+                                        return;
+                                    }
+
+                                    foreach (SyndicationItem item in feedContent.Items)
+                                    {
+                                        string quota_total = string.Empty;
+                                        string quota_usage = string.Empty;
+                                        try
+                                        {
+                                            using (WebClient wb = new WebClient())
+                                            {
+                                                string responseInString = wb.DownloadString("https://graph.facebook.com/" + Properties.Settings.Default.InstagramPageID + "/content_publishing_limit?fields=config&access_token=" + Properties.Settings.Default.InstagramToken);
+
+                                                JObject jdata = JObject.Parse(responseInString);
+                                                JToken array = jdata["data"];
+                                                foreach (JToken info in array)
+                                                {
+                                                    quota_total = info["config"]["quota_total"].ToString();
+                                                }
+                                            }
+
+                                            using (WebClient wb = new WebClient())
+                                            {
+                                                string responseInString = wb.DownloadString("https://graph.facebook.com/" + Properties.Settings.Default.InstagramPageID + "/content_publishing_limit?fields=quota_usage&access_token=" + Properties.Settings.Default.InstagramToken);
+
+                                                JObject jdata = JObject.Parse(responseInString);
+                                                JToken array = jdata["data"];
+
+                                                foreach (JToken info in array)
+                                                {
+                                                    quota_usage = info["quota_usage"].ToString();
+                                                }
+                                            }
+                                        }
+                                        catch { }
+
+                                        int usage = Convert.ToInt32(quota_usage);
+                                        int total = Convert.ToInt32(quota_total);
+
+                                        if (usage >= total)
+                                        {
+                                            EasyLogger.Info("Instagram post quota reached!");
+                                            break;
+                                        }
+
+                                        try
+                                        {
+                                            System.Collections.ObjectModel.Collection<SyndicationLink> links = item.Links;
+
+                                            string title = item.Title.Text + " - " + links[0].Uri.ToString();
+                                            string image = ExtensionMethods.GetEnclosureUri(item);
+                                            DateTimeOffset date = item.PublishDate;
+
+                                            if (StringToDTOffset(Properties.Settings.Default.PublishDate) < date)
+                                            {
+                                                Task<string> success = PostAsync(image, title);
+                                                int posts_used = Convert.ToInt32(quota_usage);
+                                                posts_used++;
+                                                EasyLogger.Info(success.Result + Environment.NewLine + "Instagram Post usage for the day: " + posts_used + " of " + quota_total);
+
+                                                Properties.Settings.Default.PublishDate = date.ToString();
+                                                Properties.Settings.Default.Save();
+                                                Properties.Settings.Default.Reload();
+                                            }
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            EasyLogger.Error(ex);
+                                            continue;
+                                        }
                                     }
                                 }
-
-                                using (WebClient wb = new WebClient())
-                                {
-                                    string responseInString = wb.DownloadString("https://graph.facebook.com/" + Properties.Settings.Default.InstagramPageID + "/content_publishing_limit?fields=quota_usage&access_token=" + Properties.Settings.Default.InstagramToken);
-
-                                    JObject jdata = JObject.Parse(responseInString);
-                                    JToken array = jdata["data"];
-
-                                    foreach (JToken info in array)
-                                    {
-                                        quota_usage = info["quota_usage"].ToString();
-                                    }
-                                }
-                            }
-                            catch { }
-
-                            int usage = Convert.ToInt32(quota_usage);
-                            int total = Convert.ToInt32(quota_total);
-
-                            if (usage >= total)
-                            {
-                                EasyLogger.Info("Instagram post quota reached!");
-                                break;
-                            }
-
-                            try
-                            {
-                                System.Collections.ObjectModel.Collection<SyndicationLink> links = item.Links;
-
-                                string title = item.Title.Text + " - " + links[0].Uri.ToString();
-                                string image = ExtensionMethods.GetEnclosureUri(item);
-                                DateTimeOffset date = item.PublishDate;
-
-                                if (StringToDTOffset(Properties.Settings.Default.PublishDate) < date)
-                                {
-                                    Task<string> success = PostAsync(image, title);
-                                    int posts_used = Convert.ToInt32(quota_usage);
-                                    posts_used++;
-                                    EasyLogger.Info(success.Result + Environment.NewLine + "Instagram Post usage for the day: " + posts_used + " of " + quota_total);
-
-                                    Properties.Settings.Default.PublishDate = date.ToString();
-                                    Properties.Settings.Default.Save();
-                                    Properties.Settings.Default.Reload();
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                EasyLogger.Error(ex);
-                                continue;
                             }
                         }
                     }
